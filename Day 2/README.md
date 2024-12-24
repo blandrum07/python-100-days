da00f90a648667d7be9d5e17ded03bab6e0bb648
Brandon Landrum <138055879+blandrum07@users.noreply.github.com>
2024-12-23 22:53:50 -0600

    Day 2 commit

---
+++ Day 2/README.md
@@ -0,0 +1,5 @@
+# Day 2
+
+Day 2 the of python-100-days challenge
+
+Simple script that takes some user input and displays it.
---
+++ Day 2/taking_user_input.py
@@ -0,0 +1,11 @@
+# Take some input from the user and display it
+
+name = ""
+age = 0
+siblings = 0
+
+name = input("Hello! What's your name?\n")
+age = input("And how old are you " + name + " ?\n")
+siblings = input("How many siblings do you have?\n")
+
+print("Your name is " + name + ", you are " + age + " years old, and you have " + siblings + " siblings.")
